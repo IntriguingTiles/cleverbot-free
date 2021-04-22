61839c631b4d0c370456bfcd605cda2e31d1a1c9
Lukáš Hájek <lukhajek007@gmail.com>
2021-04-22 09:03:48 +0200

    Added test script

---
+++ test.js
@@ -0,0 +1,9 @@
+const cleverbot = require("./index.js");
+
+cleverbot("Hi").then(response => {
+    console.log(`Response: ${response}`);
+    process.exit((response) ? 0 : 1);
+}).catch(error => {
+    console.error(error);
+    process.exit(1);
+})
