8d5b5ce424635320cc4889edc41a3a1b12c82fd1
IntriguingTiles <jnickw@outlook.com>
2021-10-17 14:26:34 -0600

    Add a simple example

---
+++ example.js
@@ -0,0 +1,22 @@
+const cb = require("./index.js");
+const rl = require("readline").createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const context = [];
+
+rl.on("line", async line => {
+    try {
+        const response = await cb(line, context);
+        context.push(line);
+        context.push(response);
+        console.log(response);
+    } catch (err) {
+        console.log(err.message);
+        console.log("Failed to get a response!");
+    }
+    rl.prompt();
+});
+
+rl.prompt();
