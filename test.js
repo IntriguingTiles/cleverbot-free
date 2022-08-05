2ac64986d3e0d1a91b4ef72561122597cd6fff31
IntriguingTiles <jnickw@outlook.com>
2022-08-05 00:29:39 -0600

    CI: Retry a couple times before erroring out

--- test.js
+++ test.js
@@ -2,18 +2,25 @@ const cleverbot = require("./index.js");
 
 
 async function go() {
-    try {
-        let response = await cleverbot("Hi.");
-        console.log(`Response: ${response}`);
-        if (!response) process.exit(1);
-        response = await cleverbot("Goodbye.");
-        console.log(`Response: ${response}`);
-        if (!response) process.exit(1);
-        process.exit(0);
-    } catch (err) {
-        console.error(err);
-        process.exit(1);
+    let retries = 0;
+
+    while (retries < 5) {
+        try {
+            let response = await cleverbot("Hi.");
+            console.log(`Response: ${response}`);
+            if (!response) process.exit(1);
+            response = await cleverbot("Goodbye.", ["Hi.", response]);
+            console.log(`Response: ${response}`);
+            if (!response) process.exit(1);
+            process.exit(0);
+        } catch (err) {
+            retries++;
+            console.error(err);
+        }
     }
+
+    console.error("Exceeded retry limit.");
+    process.exit(1);
 }
 
 go();
