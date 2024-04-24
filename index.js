dd60cb947eadd86094f9d1f7c193890001e84118
IntriguingTiles <jnickw@outlook.com>
2024-04-23 21:25:48 -0600

    Update user agent

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const superagent = require("superagent");
 const md5 = require("md5");
 
-const userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36";
+const userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36";
 
 let cookies;
 let cbsid;
